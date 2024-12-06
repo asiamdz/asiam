c0e9e0fafd117ed7db47e860d092f2a43463d6e9
asiamdz <asiam.soc.dz@gmail.com>
2024-12-06 08:53:16 +0100

    Update cms.js

--- src/js/cms.js
+++ src/js/cms.js
@@ -6,16 +6,16 @@ import styles from "!to-string-loader!css-loader!postcss-loader!sass-loader!../c
 
 import HomePreview from "./cms-preview-templates/home";
 import PostPreview from "./cms-preview-templates/post";
-import ProductsPreview from "./cms-preview-templates/products";
-import ValuesPreview from "./cms-preview-templates/values";
-import ContactPreview from "./cms-preview-templates/contact";
+import NewsPreview from "./cms-preview-templates/news";
+import ConfsPreview from "./cms-preview-templates/confs";
+import AboutusPreview from "./cms-preview-templates/aboutus";
 import FooterPreview from "./cms-preview-templates/footer";
 
 CMS.registerPreviewStyle(styles, {raw: true});
 CMS.registerPreviewTemplate("home", HomePreview);
 CMS.registerPreviewTemplate("post", PostPreview);
-CMS.registerPreviewTemplate("products", ProductsPreview);
-CMS.registerPreviewTemplate("values", ValuesPreview);
-CMS.registerPreviewTemplate("contact", ContactPreview);
+CMS.registerPreviewTemplate("news", NewsPreview);
+CMS.registerPreviewTemplate("confs", ConfsPreview);
+CMS.registerPreviewTemplate("aboutus", AboutusPreview);
 CMS.registerPreviewTemplate("footer", FooterPreview);
 CMS.init();
